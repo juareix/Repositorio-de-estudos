e9bda8e78a9d845d32ad3e6191ce0fbe563c3fb9
juareix <juarezmaciel45@gmail.com>
2022-11-16 21:04:36 -0400

    exercise

---
+++ JavaScript/2-arrays/exe1.js
@@ -0,0 +1,10 @@
+function arrOrder(arr){
+    for(let i = 0; i<arr.length; i++){
+        
+    }
+    console.log(arr)
+}
+
+let arr = [199 , 2]
+
+arrOrder(arr)
---
+++ JavaScript/2-arrays/op-basica.js
@@ -0,0 +1,21 @@
+let color = ['verde' , 'azul']
+
+// retorna o tamanho do array
+console.log(color.length)
+
+// O método JavaScript toString()converte uma matriz em uma string de valores de matriz (separados por vírgula).
+
+/*O join()método também une todos os elementos do array em uma string.
+
+Ele se comporta exatamente como toString(), mas além disso você pode especificar o separador */
+
+//O pop()método remove o último elemento de uma matriz
+
+//O push()método adiciona um novo elemento a um array (no final)
+
+/*O shift()método remove o primeiro elemento da matriz e "desloca" todos os outros elementos para um índice inferior */
+
+/*O unshift()método adiciona um novo elemento a uma matriz (no início) e "desloca" os elementos mais antigos */
+
+//O concat()método cria um novo array mesclando (concatenando) arrays existentes
+
