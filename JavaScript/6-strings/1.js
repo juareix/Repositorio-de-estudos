55c70fa0ec4a62c624db7cdd4451253f1e0f279d
juareix <juarezmaciel45@gmail.com>
2022-11-22 10:16:56 -0400

    strings an numbers

---
+++ JavaScript/6-strings/1.js
@@ -0,0 +1,23 @@
+let nome = 'juarez maciel'
+//retorna o tamanho da string
+console.log(nome.length)
+
+//procura se há o que foi especificado: se napo houver retorna -1, se houver ela retorna a posição da onde ela começa
+let resultado = nome.indexOf('maciel')
+console.log(resultado)
+
+nome.indexOf()
+
+nome.slice()
+
+nome.substring()
+
+nome.toLocaleLowerCase()
+
+nome.toUpperCase()
+
+nome.concat()
+
+nome.trim()
+
+nome.charAt()
---
+++ JavaScript/7-numbers/1.js
@@ -0,0 +1,13 @@
+let n = 456.9080876780;
+
+//transfrma em string
+newn = n.toString()
+console.log(newn)
+
+//corta as casas decimais
+newF = n.toFixed(3)
+console.log(newF)
+
+//transforma em inteiro
+newI = parseInt(n)
+console.log(newI)
